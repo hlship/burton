1d7683c3596034c71495aebb3940f2dd8ef1d78b
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-05-18 16:15:24 -0700

    Add file selection dialog

--- src/main/burton/main.cljs
+++ src/main/burton/main.cljs
@@ -4,15 +4,15 @@
 (def app            (.-app electron))
 (def browser-window (.-BrowserWindow electron))
 
-(def main-window (atom nil))
+(def *main-window (atom nil))
 
 (defn init-browser []
-  (reset! main-window (browser-window.
+  (reset! *main-window (browser-window.
                         (clj->js {:width 800
                                   :height 600})))
   ; Path is relative to the compiled js file (main.js in our case)
-  (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
-  (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil)))
+  (.loadURL ^js/electron.BrowserWindow @*main-window (str "file://" js/__dirname "/public/index.html"))
+  (.on ^js/electron.BrowserWindow @*main-window "closed" #(reset! *main-window nil)))
 
 ; CrashReporter can just be omitted
 
--- src/ui/burton/ui.cljs
+++ src/ui/burton/ui.cljs
@@ -1,11 +1,15 @@
 (ns burton.ui
   (:require
     [reagent.core :as reagent :refer [atom]]
+    [burton.main :refer [*main-window]]
     [burton.plex :refer [plex-component]]
     [re-frame.core :as rf])
   (:require-macros [burton.utils :as u]))
 
 (enable-console-print!)
+
+(def electron (js/require "electron"))
+
 
 (defn root-component
   []
@@ -17,16 +21,15 @@
         path)
       [:form.form-inline
        [:button.btn {:type :submit
-                     :on-click (u/dispatch [:load-file])} "Load"]]]
+                     :on-click (u/dispatch [:select-file])} "Load"]]]
      [plex-component]
      ;; Temporary:
      (when @*loaded-file
        [:a.btn.btn-primary.btn-block {:on-click (u/dispatch [::unload-file])} "Reset"])]))
 
-(rf/reg-event-fx :load-file
-                 []
-                 (fn [{:keys [db]} _ [_]]
-                   {:db (assoc db ::loaded-file "placeholder.edn")}))
+(rf/reg-event-db :load-file
+                 (fn [db [_ file-name]]
+                   (assoc db ::loaded-file file-name)))
 
 ;; Temporary:
 (rf/reg-event-db ::unload-file
@@ -37,6 +40,19 @@
   :loaded-file
   (fn [db _]
     (::loaded-file db)))
+
+(rf/reg-event-fx
+  :select-file
+  (fn [_ _]
+    (-> electron
+        .-remote
+        .-dialog
+        (.showOpenDialog @*main-window
+                         (clj->js {:title "Open Dependency File"
+                                   :filters [{:name "EDN" :extensions ["edn"]}]
+                                   :properties ["openFile" "showHiddenFiles"]})
+                         (fn [filename]
+                           (rf/dispatch [:load-file filename]))))))
 
 (reagent/render
   [root-component]
