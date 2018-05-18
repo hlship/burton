6ceae9f55b93a351be256db7d70c6f78c6e12833
Howard M. Lewis Ship <hlship@walmartlabs.com>
2018-05-18 13:28:54 -0700

    Switch to Bootstrap 4.1.1

---
+++ src/ui/burton/plex.cljs
@@ -0,0 +1,9 @@
+(ns burton.plex
+  "Drawing inspiration from TheBrain.")
+
+(defn plex-component
+  []
+  [:div.jumbotron.jumpotron-fluid
+   [:div.alert.alert-primary {:role :alert}
+    "No document has been loaded."
+    ]])
--- src/ui/burton/ui.cljs
+++ src/ui/burton/ui.cljs
@@ -1,21 +1,24 @@
 (ns burton.ui
   (:require
-    [reagent.core :as reagent :refer [atom]]))
+    [reagent.core :as reagent :refer [atom]]
+    [burton.plex :refer [plex-component]]
+    [re-frame.core :as rf]))
 
 (enable-console-print!)
 
 (defn root-component []
-  [:div
-   [:div.logos
-    [:img.electron {:src "img/electron-logo.png"}]
-    [:img.cljs {:src "img/cljs-logo.svg"}]
-    [:img.reagent {:src "img/reagent-logo.png"}]]
-   [:pre "Versions:"
-    [:p (str "Node     " js/process.version)]
-    [:p (str "Electron " ((js->clj js/process.versions) "electron"))]
-    [:p (str "Chromium " ((js->clj js/process.versions) "chrome"))]]])
-
+  [:div.container-fluid
+   [:nav.navbar.navbar-expand-lg.navbar-dark.bg-primary
+    [:a.navbar-brand {:href "#"} "Burton"]
+    [:form.form-inline
+     [:button.btn {:type :submit} "Load"]]]
+   (plex-component)])
 
 (reagent/render
   [root-component]
   (js/document.getElementById "app-container"))
+
+(rf/reg-event-db :initialize (fn [_ _]
+                               {:file-path nil}))
+(rf/dispatch-sync [:initialize])
+
